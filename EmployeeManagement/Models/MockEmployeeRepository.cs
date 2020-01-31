d8fc00be39168a12264ab21ff585dedc67e30019
sangtm-04 <sangtran.d14ptit@gmail.com>
2020-01-31 23:25:42 +0700

    Using tag helpers

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -13,9 +13,9 @@ namespace EmployeeManagement.Models
         {
             _employees = new List<Employee>()
             {
-                new Employee() {Id = 1, Name = "Sang", Email="sang@gmail.com", Department = "IT"},
-                new Employee() {Id = 2, Name = "Hung", Email="hung@gmail.com", Department = "HR"},
-                new Employee() {Id = 3, Name = "Giang", Email="giang@gmail.com", Department = "IT"}
+                new Employee() {Id = 1, Name = "Sang", Email="sang@gmail.com", Department = Dept.IT},
+                new Employee() {Id = 2, Name = "Hung", Email="hung@gmail.com", Department = Dept.HR},
+                new Employee() {Id = 3, Name = "Giang", Email="giang@gmail.com", Department = Dept.IT}
             };
         }
 
